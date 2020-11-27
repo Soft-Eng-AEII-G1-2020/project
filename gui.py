95666d94938d35842adfac3fb1d14385a2290588
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2020-11-27 13:23:54 +0000

    fix(autopep8): Automated autopep8 fixes

--- gui.py
+++ gui.py
@@ -2,6 +2,7 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 
+
 class Window(QMainWindow):
 
     def __init__(self, parent=None):
@@ -10,7 +11,7 @@ class Window(QMainWindow):
         self.gridTop.setSpacing(10)
         self.grid = QGridLayout()
         self.grid.setSpacing(10)
-        self.setWindowTitle("Binary files converter") 
+        self.setWindowTitle("Binary files converter")
 
         self.w = QWidget()
         self.vb = QVBoxLayout()
@@ -26,7 +27,7 @@ class Window(QMainWindow):
         self.setFixedWidth(500)
         self.setFixedHeight(500)
 
-    def setMenu(self):     
+    def setMenu(self):
         self.menu = self.menuBar()
         self.fileMenu = self.menu.addMenu("Menu")
         self.fileMenu.addAction('Exit \t Esc', self.close)
@@ -36,35 +37,35 @@ class Window(QMainWindow):
     def setGui(self):
         self.inText = QPlainTextEdit()
         self.outText = QPlainTextEdit()
-        self.grid.addWidget(self.inText,0,0)
-        self.grid.addWidget(self.outText,0,1)
+        self.grid.addWidget(self.inText, 0, 0)
+        self.grid.addWidget(self.outText, 0, 1)
         self.btnOne = QPushButton("Open File")
         self.btnMany = QPushButton("Open Many")
         self.btnSave = QPushButton("Save")
         self.btnConvert = QPushButton("Convert")
         self.btnReset = QPushButton("Reset")
-        self.gridTop.addWidget(self.btnOne,0,0)
-        self.gridTop.addWidget(self.btnMany,0,1)
+        self.gridTop.addWidget(self.btnOne, 0, 0)
+        self.gridTop.addWidget(self.btnMany, 0, 1)
 
     def step1Buttons(self):
-        for i in reversed(range(self.grid.count())): 
+        for i in reversed(range(self.grid.count())):
             self.gridTop.itemAt(i).widget().setParent(None)
         self.inText.setPlainText("")
         self.outText.setPlainText("")
-        self.gridTop.addWidget(self.btnOne,0,0)
-        self.gridTop.addWidget(self.btnMany,0,1)
+        self.gridTop.addWidget(self.btnOne, 0, 0)
+        self.gridTop.addWidget(self.btnMany, 0, 1)
 
     def step2Buttons(self):
-        for i in reversed(range(self.grid.count())): 
+        for i in reversed(range(self.grid.count())):
             self.gridTop.itemAt(i).widget().setParent(None)
-        self.gridTop.addWidget(self.btnReset,0,0)
-        self.gridTop.addWidget(self.btnConvert,0,1)
+        self.gridTop.addWidget(self.btnReset, 0, 0)
+        self.gridTop.addWidget(self.btnConvert, 0, 1)
 
     def step3Buttons(self):
-        for i in reversed(range(self.grid.count())): 
+        for i in reversed(range(self.grid.count())):
             self.gridTop.itemAt(i).widget().setParent(None)
-        self.gridTop.addWidget(self.btnReset,0,0)
-        self.gridTop.addWidget(self.btnSave,0,1)
+        self.gridTop.addWidget(self.btnReset, 0, 0)
+        self.gridTop.addWidget(self.btnSave, 0, 1)
 
     def bindButtons(self):
         self.btnReset.clicked.connect(self.step1Buttons)
@@ -74,16 +75,18 @@ class Window(QMainWindow):
 
     def startProcesing(self):
         self.step3Buttons()
-        self.outText.setPlainText( self.inText.toPlainText())
+        self.outText.setPlainText(self.inText.toPlainText())
 
     def openAFile(self):
-        self.fileName, selectedFilter = QFileDialog.getOpenFileName(self.w, "Select a binary file", "Initial file name", "text file (*.txt)")
+        self.fileName, selectedFilter = QFileDialog.getOpenFileName(
+            self.w, "Select a binary file", "Initial file name", "text file (*.txt)")
         fileIn = open(self.fileName, "r")
         self.inText.setPlainText(fileIn.read())
         self.step2Buttons()
 
     def saveAFile(self):
-        self.fileName, selectedFilter = QFileDialog.getSaveFileName(self.w, "Select a text file", "", "text file (*.txt)")
+        self.fileName, selectedFilter = QFileDialog.getSaveFileName(
+            self.w, "Select a text file", "", "text file (*.txt)")
         fileOut = open(self.fileName, "w+")
         fileOut.write(self.outText.toPlainText())
 
