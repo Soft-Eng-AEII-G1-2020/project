398d68705e3e92d4f7e706706194ee188ae02ca9
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2020-12-10 18:00:29 +0100

    test: added tests for new methods

--- binary_conversion_test.py
+++ binary_conversion_test.py
@@ -1,17 +1,23 @@
 import binary_conversion as bc
 import pytest
 
-PROPER_DATA = r'23 20 42 79 74 65 2D 63 6F 6D 70 69 6C 65 64'
-TRANSLATED = '# Byte-compiled'
-IMPROPER_DATA = r'23 20 42 79 74 65 ZZ 63 6F 6D 70 69 6C 65 64'
+PROPER_DATA = r'00100011 00100000 01000010 01111001 01110100 01100101 00101101 01100011 01101111 01101101 01110000 01101001 01101100 01100101 01100100'
+IMPROPER_DATA = r'00100012 00100000 01000010 01111001 01110100 01100101 00101101 01100011 01101111 01101101 01110000 01101001 01101100 01100101 01100100'
+HEX = r'23 20 42 79 74 65 2D 63 6F 6D 70 69 6C 65 64 '
+DEC = r'035 032 066 121 116 101 045 099 111 109 112 105 108 101 100 '
+OCT = r'043 040 102 171 164 145 055 143 157 155 160 151 154 145 144 '
+ASCII = r'# Byte-compiled'
 
 
 def test_convert():
     conv = bc.BinConverter()
 
-    data = conv.convert(PROPER_DATA)
+    out = [ASCII, HEX, DEC, OCT]
 
-    assert data == TRANSLATED
+    for i in range(0,4):
+        data = conv.convert(PROPER_DATA, i)
 
-    with pytest.raises(ValueError, match='.*ZZ.*'):
-        data = conv.convert(IMPROPER_DATA)
+        assert data == out[i]
+
+        with pytest.raises(ValueError, match='.*2.*'):
+            data = conv.convert(IMPROPER_DATA, i)
